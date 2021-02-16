b0c68da1a9e7374e6916df7733b5357005e1d217
Alexandre VOGEL <alexandre.vogel@etu.unistra.fr>
2021-02-16 12:09:36 +0100

    start user controllers

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,26 @@
-exports.add = (req, res, next) => {
+const dbConfig = require('../config/dbConfig');
+const knex = require('knex')(dbConfig);
+const crypto = require('crypto');
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
 
+exports.add = (req, res, next) => {
+    knex('users').insert({
+        idUser: knex.raw('NEXTVAL(s_users)'),
+        username: req.body['username'],
+        pwd: bcrypt.hashSync(req.body['password'], saltRounds),
+        email: req.body['email'],
+    })
+    .then( () => {
+        res.status(200).send('OK');
+    })
+    .catch((err) => {
+        if (err.code === 'ER_DUP_ENTRY') {
+            res.status(401).send("L'utilisateur existe déjà");
+        } else {
+            res.status(400).send('Bad');
+        }
+    });
 }
 
 exports.remove = (req, res, next) => {
@@ -7,7 +28,32 @@ exports.remove = (req, res, next) => {
 }
 
 exports.login = (req, res, next) => {
-    
+    knex('users').select('idUser', 'pwd').where({username: req.body['username']})
+    .then((data) => {
+        if (bcrypt.compareSync(req.body['password'] ,data[0].pwd)) {
+            let token = crypto.randomBytes(40).toString('hex');
+            knex('tokens').insert({
+                idToken: knex.raw('NEXTVAL(s_users)'),
+                idUser: data[0].idUser,
+                strToken: token,
+                expirationDate: knex.raw('SYSDATE()+1') // not work
+            }).then(()=> {
+                res.status(200).json({
+                    token: token
+                });
+            })
+            .catch((err)=> {
+                console.log(err);
+                res.status(400).send('Bad');
+            })
+        } else {
+            res.status(400).send('Bad');
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(400).send('Bad');
+    });
 }
 
 exports.check = (req, res, next) => {
